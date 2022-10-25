791cc6b21a76c4bf9f742eac6836461b3284fa60
billiamchoi <billiamchoi4u@gmail.com>
2022-10-25 18:02:03 +0900

    MemberController 주석 작성 완료

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -20,37 +20,67 @@ import java.security.Principal;
 @Controller
 @RequestMapping("/account")
 public class MemberController {
+    // 회원가입, 로그인, 로그아웃은 memberService에서 처리
+    // 회원 정보 보기, 회원 정보 수정, 회원 탈퇴는 accountService에서 처리
     @Autowired
     private MemberService memberService;
 
     @Autowired
     private AccountService accountService;
 
-    @RequestMapping(value="/login")
+    // 로그인 페이지를 렌더링할 경우, 사용하는 controller
+    // url: /account/login
+    // method: GET
+    // 사용하는 위치: 사용자가 네비게이션 바의 Login 버튼
+    // pageTitle : 로그인
+    @GetMapping(value="/login")
     public String loginView(ModelMap model) {
-        model.addAttribute("pageTitle", "로그인 페이지");
+        model.addAttribute("pageTitle", "로그인");
         
         return "/account/login";
     }
 
-    @RequestMapping(value = "/signup")
+    // 회원가입 페이지를 렌더링할 경우, 사용하는 controller
+    // url: /account/signup
+    // method: GET
+    // 사용하는 위치: 사용자가 네비게이션 바의 Sign Up 버튼
+    // pageTitle : 회원가입
+    @GetMapping(value = "/signup")
     public String signupView(MemberDTO memberDTO, ModelMap model) {
-        model.addAttribute("pageTitle", "회원가입 페이지");
+        model.addAttribute("pageTitle", "회원가입");
         return "/account/signup";
     }
 
+    // 로그인 성공 페이지를 렌더링할 경우, 사용하는 controller
+    // url: /account/login/result
+    // method: GET
+    // 사용하는 위치: 로그인 버튼
+    // pageTitle : 로그인 성공
     @GetMapping("/login/result")
     public String LoginResultView(ModelMap model) {
         model.addAttribute("pageTitle", "로그인 성공");
         return "/account/loginSuccess";
     }
 
+    // 로그아웃 성공 페이지를 렌더링할 경우, 사용하는 controller
+    // url: /account/logout/result
+    // method: GET
+    // 사용하는 위치: 네비게이션 바의 Logout 버튼
+    // pageTitle : 로그아웃 성공
     @GetMapping("/logout/result")
     public String LogoutResultView(ModelMap model) {
         model.addAttribute("pageTitle", "로그아웃 성공");
         return "/account/logoutSuccess";
     }
 
+    // 회원가입 로직을 담당하는 controller
+    // url: /account/signup
+    // method: POST
+    // 사용하는 위치: 회원가입 페이지의 회원가입 버튼
+    // bindingResult를 사용하여 화면에서 form의 th:object로 memberDTO 객체를 받아
+    // 회원가입 validation을 구현
+    // 1. 패스워드와 패스워드 확인 일치 여부
+    // 2. 사용자ID 중복 확인
     @PostMapping("/signup")
     public String signup( @Valid MemberDTO memberDTO, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
@@ -78,6 +108,11 @@ public class MemberController {
         return "redirect:/account/login";
     }
 
+    // 내 정보 페이지를 렌더링할 경우, 사용하는 controller
+    // url: /account/info
+    // method: GET
+    // 사용하는 위치: 네비게이션 바의 MyInfo 버튼
+    // pageTitle : 내 정보
     @GetMapping("/info")
     public String myInfoView(Principal principal, ModelMap model) {
 
@@ -94,6 +129,13 @@ public class MemberController {
         return "/account/myinfo";
     }
 
+    // 내 정보 수정페이지를 렌더링할 경우, 사용하는 controller
+    // url: /account/modify
+    // method: GET
+    // 사용하는 위치: 내 정보 페이지 하단에 수정하기 버튼
+    // pageTitle : 내 정보 수정
+    // MemberModifyDTO 클래스를 별도로 만듬 (이유: 현재 비밀번호, 새로운 비밀번호, 새로운 비밀번호 확인을 구현하기 위해 객체에 하나의 변수를 추가해야 하기 때문)
+    // 내 정보 수정 페이지에서 th:object로 설정할 memberModifyDTO를 여기서 가져와서 로그인한 유저의 id, 사용자ID, 이메일을 set함
     @GetMapping("/modify")
     public String myInfoModifyView(Principal principal, MemberModifyDTO memberModifyDTO, ModelMap model) {
 
@@ -114,6 +156,14 @@ public class MemberController {
         return "/account/modify";
     }
 
+    // 내 정보 수정 로직을 담당하는 controller
+    // url: /account/modify
+    // method: POST
+    // 사용하는 위치: 내 정보 수정 하단 수정하기 버튼
+    // bindingResult를 사용하여 화면에서 form의 th:object로 memberModifyDTO 객체를 받아
+    // 내 정보 수정 validation을 구현
+    // 1. 현재 비밀번호 일치 여부
+    // 2. 새로운 패스워드와 새로운 패스워드 확인 일치 여부
     @PostMapping("/modify")
     public String myInfoModify(Principal principal, @Valid MemberModifyDTO memberModifyDTO, BindingResult bindingResult) {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
@@ -142,25 +192,15 @@ public class MemberController {
             return "/account/modify";
         }
 
-        if(!principal.getName().equals(memberDTO.getUsername())) {
-            try{
-                memberService.joinUser(memberDTO);
-            }catch (DataIntegrityViolationException e) {
-                e.printStackTrace();
-                bindingResult.reject("modifyFailed", "이미 등록된 사용자입니다.");
-                return "/account/modify";
-            }catch (Exception e) {
-                e.printStackTrace();
-                bindingResult.reject("modifyFailed", e.getMessage());
-                return "/account/modify";
-            }
-        }
-
         accountService.modify(memberModifyDTO);
 
         return "redirect:/account/info";
     }
 
+    // 회원 삭제 로직을 담당하는 controller
+    // url: /account/delete
+    // method: POST
+    // 사용하는 위치: 현재 없음
     @PostMapping("/delete")
     public String memberRemove(@RequestParam ("id") Long id) {
 
@@ -168,6 +208,11 @@ public class MemberController {
         return "redirect:/account/logout";
     }
 
+    // 회원 탈퇴 로직을 담당하는 controller
+    // url: /account/withdrawal
+    // method: POST
+    // 사용하는 위치: 회원 정보 하단 회원 탈퇴 버튼
+    // HttpSession session으로 삭제시 세션 종료 구현
     @PostMapping("/withdrawal")
     public String memberWithdrawl(HttpSession session, @RequestParam("id") Long id) {
 
